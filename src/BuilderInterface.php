db9a6be9807560f794c8ecdad2747616a6452181
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-08-14 17:11:55 +0300

    add interface

---
+++ src/BuilderInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace kuaukutsu\queue\core;
+
+use kuaukutsu\queue\core\interceptor\InterceptorInterface;
+
+/**
+ * @api
+ */
+interface BuilderInterface
+{
+    public function withInterceptors(InterceptorInterface ...$interceptor): self;
+
+    public function buildPublisher(): PublisherInterface;
+
+    public function buildConsumer(): ConsumerInterface;
+}
