ef7b0528fde2b013c60ecc14991c09ee781cb985
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-08-15 10:38:08 +0300

    Merge pull request #5
    * publisher batch

--- src/PublisherInterface.php
+++ src/PublisherInterface.php
@@ -15,4 +15,11 @@ interface PublisherInterface
      * @throws QueuePublishException
      */
     public function push(SchemaInterface $schema, QueueTask $task, ?QueueContext $context = null): string;
+
+    /**
+     * @param list<QueueTask> $taskBatch
+     * @return list<non-empty-string>
+     * @throws QueuePublishException
+     */
+    public function pushBatch(SchemaInterface $schema, array $taskBatch, ?QueueContext $context = null): array;
 }
