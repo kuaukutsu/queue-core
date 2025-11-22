b990a99636ebeb50e460939de56130f82f4d36e5
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-11-22 14:12:56 +0300

    message attempts default

--- src/QueueContext.php
+++ src/QueueContext.php
@@ -33,7 +33,7 @@ final readonly class QueueContext implements Serializable
     public static function make(SchemaInterface $schema): self
     {
         return new self(
-            attempt: 0,
+            attempt: 1,
             timeout: 0,
             routingKey: $schema->getRoutingKey(),
             createdAt: gmdate('c'),
@@ -69,10 +69,10 @@ final readonly class QueueContext implements Serializable
         );
     }
 
-    public function incrAttempt(): self
+    public function incrAttempt(?int $attempt = null): self
     {
         return new self(
-            attempt: $this->attempt + 1,
+            attempt: $attempt ?? $this->attempt + 1,
             timeout: $this->timeout,
             routingKey: $this->routingKey,
             createdAt: $this->createdAt,
